a17a5c50006ea9d609f5311bc004d2371c7977ba
ViniHGV <ixxvinicius@gmail.com>
2023-12-05 13:23:38 -0300

    feat: criando componente button da aplicação

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ButtonType } from "../../types/ButtonType";
+
+export const Button = ({ title }: ButtonType) => {
+  return (
+    <button className="text-center text-xs px-5 py-4 bg-button rounded-lg font-semibold transition-all hover:bg-button-hover">
+      {title}
+    </button>
+  );
+};
