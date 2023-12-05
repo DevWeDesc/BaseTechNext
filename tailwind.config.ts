ed045a20290bf8fa104024e35d1bc586d2a1e571
ViniHGV <ixxvinicius@gmail.com>
2023-12-05 13:20:19 -0300

    style: adicionando cores default do projeto

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      backgroundColor: {
+        input: "rgba(255, 255, 255, 0.1)",
+        "input-hover": "rgba(255, 255, 255, 0.2)",
+        button: "rgba(106, 0, 198, 0.7)",
+        "button-hover": "rgba(106, 0, 198, 1)",
+      },
+      colors: {
+        "opacity-not-focus": "rgba(255, 255, 255, 0.8)",
+        "opacity-on-focus": "rgba(255, 255, 255, 1.0)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
