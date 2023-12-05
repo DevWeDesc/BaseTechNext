20ea3bd00bf9a33810660cd2404928683ee56e64
ViniHGV <ixxvinicius@gmail.com>
2023-12-05 13:49:50 -0300

    feat: criando a pagina de criação de conta e adicionando o componente CreateAccount

---
+++ src/app/CreateAccount/page.tsx
@@ -0,0 +1,6 @@
+import { CreateAccontComponent } from "@/components/Forms/CreateAccontComponent";
+import React from "react";
+
+export default function Page() {
+  return <CreateAccontComponent />;
+}
